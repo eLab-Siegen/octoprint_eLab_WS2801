fc438b42fe60dffeff937a309acd9ecd73814c2f
Johannes Schmitt <johannes.schmitt@student.uni-siegen.de>
2019-11-13 16:12:25 +0100

    10

--- octoprint_eLab_WS2801/__init__.py
+++ octoprint_eLab_WS2801/__init__.py
@@ -69,7 +69,7 @@ class eLab_WS2801_Plugin(octoprint.plugin.EventHandlerPlugin, octoprint.plugin.P
         pixels.set_pixel((PROGRESSBAR - 1 - (progress * PROGRESSBAR / 100)), Adafruit_WS2801.RGB_to_color( 0, 255, 0 ))
         if progress == 100:
             for y in range(PROGRESSBAR):
-            pixels.set_pixel(y, Adafruit_WS2801.RGB_to_color( 255, 0, 0 ))
+                pixels.set_pixel(y, Adafruit_WS2801.RGB_to_color( 255, 0, 0 ))
         pixels.show()
     
     
