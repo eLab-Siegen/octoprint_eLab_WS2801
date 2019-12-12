9874c8c767a7bd64f2470a3d3f670f50a286f1d3
Johannes Schmitt <johannes.schmitt@student.uni-siegen.de>
2019-12-12 10:19:50 +0100

    increased default pixel count to 16

--- octoprint_eLab_WS2801/__init__.py
+++ octoprint_eLab_WS2801/__init__.py
@@ -15,7 +15,7 @@ import Adafruit_GPIO.SPI as SPI
 
 # Configure the count of pixels:
 PIXEL_COUNT = 64
-PROGRESSBAR = 10
+PROGRESSBAR = 16
 
 # Alternatively specify a hardware SPI connection on /dev/spidev0.0:
 SPI_PORT   = 0
